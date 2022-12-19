0b449275f261b20d32c04476c97ec0be4e544bc2
Chao Ma <mjc1378098@gmail.com>
2022-12-19 09:43:02 +0000

    Ini2 Tic Tac Toe

--- script.js
+++ script.js
@@ -2,7 +2,23 @@
 
 
 document.addEventListener('DOMContentLoaded', () => {
+    const gameCase = document.querySelectorAll(".case");
+    let player = document.getElementById("player");
+    const winnerMessage = document.querySelector("#messageWinner>span")
 
-   
+    //   console.log(gameCase)
+    //   console.log(player)
+    //   console.log(winnerMessage)
 
+    function render() {
+
+    }
+
+    function check() {
+
+    }
+
+    function generateEvent() {
+
+    }
 })
