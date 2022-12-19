6f3e870b3bbc14ee5f04f7172ba44fd6411294cb
Chao Ma <mjc1378098@gmail.com>
2022-12-19 13:20:54 +0000

    Tic Tac Toe fonctionnel

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 document.addEventListener('DOMContentLoaded', () => {
     const gameCase = document.querySelectorAll(".case");
     let player = document.getElementById("player");
-    const winnerMessage = document.querySelector("#messageWinner>span")
+    const winnerMessage = document.querySelector(".messageWinner")
+    console.log(winnerMessage)
     const restart = document.getElementById("restart")
     let tour = 0;
     const checkWin = ["","","","","","","","",""]
@@ -13,17 +14,45 @@ document.addEventListener('DOMContentLoaded', () => {
     //   console.log(player)
     //   console.log(winnerMessage)
 
-    function render(winner) {
-        winnerMessage.inner = winner
-    }
+    // function render(winner) {
+    //     winnerMessage.inner = winner
+    // }
 
-    function check() {
+    function checkNumber() {
+        
+        
         for(let i = 0;i<winCondition.length;i++ ){
-            for(let j = 0;j<winCondition[i].length;j++){
-                
+            const check = {
+            first:"",
+            second:"",
+            third: ""
             }
+            if(checkWin[winCondition[i][0]] === "X"){
+              check.first = true;  
+            } else if(checkWin[winCondition[i][0]] === "O"){
+                check.first = false;
+            }
+            if(checkWin[winCondition[i][1]] === "X"){
+              check.second = true;  
+            } else if(checkWin[winCondition[i][1]] === "O"){
+                check.second = false;
+            }
+            if(checkWin[winCondition[i][2]] === "X"){
+              check.third = true;  
+            } else if(checkWin[winCondition[i][2]] === "O"){
+                check.third = false;
+            }
+            checkWinner(check.first,check.second,check.third)
         } 
-        //  if()   
+    }
+    function checkWinner(first,second,third){
+        if(first === true && second===true && third===true){
+             winnerMessage.classList.add("win")
+             winnerMessage.innerHTML = "player1 a gagné"
+         }else if (first === false && second===false && third===false){
+             winnerMessage.classList.add("win")
+             winnerMessage.innerHTML = "player2 a gagné"
+         }   
     }
 
     function generateEvent() {
@@ -33,18 +62,19 @@ document.addEventListener('DOMContentLoaded', () => {
                     return console.log("Déja pris")
                 }
                 else if (tour % 2 === 0) {
-                    gameCase[i - 1].innerHTML ="O"
-                    player.innerHTML = "player2"
-                    checkWin[i-1] = "O";
-                    tour++
-                }
-                else {
                     gameCase[i - 1].innerHTML ="X"
-                    player.innerHTML = "player1"
+                    player.innerHTML = "player2"
                     checkWin[i-1] = "X";
                     tour++
                 }
-                console.log(checkWin)
+                else {
+                    gameCase[i - 1].innerHTML ="O"
+                    player.innerHTML = "player1"
+                    checkWin[i-1] = "O";
+                    tour++
+                }
+                checkNumber()
+
             })
         }
     }
